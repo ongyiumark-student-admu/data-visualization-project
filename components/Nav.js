af0ba056b805c4a80f6d75bfb70cfc7b26338d41
Mark Kevin A. Ong Yiu <markkevin.ongyiu@gmail.com>
2023-07-10 23:55:49 +0800

    added snaps

--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,21 @@
 export default function Nav( {genreFocus} ) {
   return (
     <>
-    <div className="flex-row-center bg-slate-900 text-white w-full h-fit sticky top-0 z-20 mb-10">
+    {console.log(genreFocus)}
+    <div className="flex-row-center bg-slate-900 text-white w-full h-16 sticky top-0 z-20 mb-10">
       <div className="flex-justify-start w-1/2 py-3 px-5">
         <h1 className="font-extrabold text-xl shrink">
-        <a href="/" className="font-extrabold text-xl shrink hover:text-stone-300">Data Story</a>
+        <ul className="flex-justify-start">
+        <li className="flex-row-center"><a href="/" className="font-extrabold text-xl shrink hover:text-stone-300 mx-3">
+          Home</a></li>
+          <li className="flex-row-center"><a href="/Indie" className="font-extrabold text-xl shrink hover:text-stone-300 mx-3">
+          Data Story</a></li>
+        </ul>
+
         </h1>
       </div>
       <div className="flex-justify-end w-1/2 py-3 px-5">
-        <h1 className="text-slate-400 text-sm shrink">
+        <h1 className="text-slate-400 text-xs shrink resize">
           <a
             href="https://github.com/ongyiumark"
             className="hover:text-slate-500"
