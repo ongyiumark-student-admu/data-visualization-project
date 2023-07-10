6c7f4ef3b3aa5dbd49cc2b1018060bf38d7beea1
Mark Kevin A. Ong Yiu <55453765+ongyiumark@users.noreply.github.com>
2023-07-10 13:05:06 +0800

    Revert "Development"

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,7 @@ import Nav from "../components/Nav.js";
 import clientPromise from "../lib/mongodb";
 import { useState } from "react";
 
-import dynamic from "next/dynamic";
-const ApexCharts = dynamic(() => import("react-apexcharts"), { ssr: false });
-import { getGradientColor, linspace } from "../lib/utilities.js";
-
-export default function Home({ genres, heatmapData, heatmapOptions }) {
+export default function Home({ genres }) {
   const [genreSearchInput, setGenreSearchInput] = useState("");
 
   const handleChange = (e) => {
@@ -27,26 +23,6 @@ export default function Home({ genres, heatmapData, heatmapOptions }) {
         <title>Home</title>
       </Head>
       <Nav />
-
-      
-      <div className="flex-col-center my-12 w-full">
-          <div className="flex-col-center w-4/5">
-            <h1 className="text-4xl font-extrabold text-royalblue">
-              Steam Game Genres
-            </h1>
-            <p className="text-base">Yearly Count from 2009 to 2022</p>
-            <p className="instructions">You may hover to inspect the value</p>
-            <ApexCharts
-              className="flex-row-center w-full"
-              options={heatmapOptions}
-              series={heatmapData}
-              type="heatmap"
-              height={600}
-            />
-
-          </div>
-        </div>
-
       <div className="mx-12 mb-24 w-4/5 mx-auto article">
         <h1>Instructions</h1>
         <p>
@@ -67,7 +43,6 @@ export default function Home({ genres, heatmapData, heatmapOptions }) {
             Indie Steam Games
           </div>
         </a>
-
 
         <h1>All Genres</h1>
         <p>
@@ -123,112 +98,18 @@ export async function getServerSideProps() {
     const client = await clientPromise;
     const db = client.db("steam_games");
 
-    let l_year = 2009;
-    let r_year = 2022;
-    let colors = ["#0504aa", "#006600"];
-    let foreColor = "#0e1629";
-
-    const gameGenres = await db
+    const genres = await db
       .collection("games")
       .aggregate([
-        {
-          $project: {
-            _id: 0,
-            converted_date: 1,
-            genres: 1,
-            year: { $year: "$converted_date" },
-          },
-        },
-        {
-          $match: {
-            $and: [{ year: { $gte: l_year } }, { year: { $lte: r_year } }],
-          },
-        },
+        { $project: { genres: 1 } },
         { $unwind: "$genres" },
-        {
-          $group: {
-            _id: { year: { $year: "$converted_date" }, genre: "$genres" },
-            games_count: { $sum: 1 },
-          },
-        },
+        { $group: { _id: { genres: "$genres" }, n: { $sum: 1 } } },
       ])
       .toArray();
 
-    // convert to apexchart format
-    let game_genres = {};
-    for (let game of gameGenres) {
-      const { genre, year } = game._id;
-      if (!(genre in game_genres)) game_genres[genre] = {};
-      game_genres[genre][year] = game.games_count;
-    }
-    for (let genre in game_genres) {
-      for (let i = l_year; i <= r_year; i++) {
-        if (!(i in game_genres[genre])) game_genres[genre][i] = 0;
-      }
-    }
-
-    // order lexicographically
-    const ordered_genres = Object.keys(game_genres)
-      .sort()
-      .reverse()
-      .reduce((obj, key) => {
-        obj[key] = game_genres[key];
-        return obj;
-      }, {});
-
-    // finish converting to apexchart format
-    let heatmap_series = [];
-    let max_val = 0;
-    for (let genre in ordered_genres) {
-      const tmp = {};
-      tmp["name"] = genre;
-      tmp["data"] = [];
-      for (let year in game_genres[genre]) {
-        tmp["data"].push({ x: year, y: game_genres[genre][year] });
-        max_val = Math.max(max_val, game_genres[genre][year]);
-      }
-      heatmap_series.push(tmp);
-    }
-
-    // options for heatmap
-    let n_ranges = 20;
-    let color_percent = linspace(0.1, 1, n_ranges);
-    let to_from = linspace(1, max_val, n_ranges);
-    let ranges = [];
-    for (let i = 0; i < n_ranges - 1; i++) {
-      let tmp = {
-        from: to_from[i],
-        to: to_from[i + 1],
-        color: getGradientColor(
-          "#FFFFFF",
-          colors[0],
-          Math.sqrt(color_percent[i + 1])
-        ),
-      };
-      ranges.push(tmp);
-    }
-    ranges.push({ from: 0, to: 0, color: "#FFFFFF" });
-
-    let heatmapOptions = {
-      chart: { type: "heatmap", foreColor: foreColor },
-      dataLabels: { enabled: false },
-      colors: [colors[0]],
-      plotOptions: {
-        heatmap: {
-          shadeIntensity: 1,
-          enableShades: false,
-          useFillColorAsStroke: false,
-          colorScale: { ranges: ranges },
-        },
-      },
-      legend: { show: false },
-    };
-
     return {
       props: {
-        genres: gameGenres.map((g) => g._id.genre).sort(),
-        heatmapData: heatmap_series,
-        heatmapOptions: heatmapOptions
+        genres: genres.map((g) => g._id.genres).sort(),
       },
     };
   } catch (e) {
